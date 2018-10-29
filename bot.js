e109d7faf72af5b785e74af1d99eed1fdc0493ad
Hiwawe <44549990+Hiwawe@users.noreply.github.com>
2018-10-29 22:18:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('ready', () => {
   client.on('ready', async () => {
       let ToRnEdO  = ["this","is","nothing"]
   setInterval(() => {
-client.channels.get("506147674068877322").send(`${ToRnEdO [Math.floor(Math.random() *ToRnEdO .length)]}`)
+client.channels.get("506545638922649614").send(`${ToRnEdO [Math.floor(Math.random() *ToRnEdO .length)]}`)
 },500);
 });
   
